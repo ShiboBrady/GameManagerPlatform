0cb576c9545f94caf877df2c5bfc193f99072cb5
张世柏 <zhangshibo706@gmail.com>
2016-01-23 18:06:13 +0800

    enhance function for page tab.
    增强了打开页面标签的功能，包括：当标签长度超过页面宽度时，会以下拉箭头的形式进行展示；当关闭了足够多的页面时，会隐藏下拉箭头；当页面大小发生变化时，相应的也会显示和隐藏的页面标签。

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,9 @@
 $(function () {
-    // console.log($("#right_part").width());
-    // $(window).resize(function(){
-    //     console.log("windows size changed, new size is: " + $("#right_part").width());
-    // });
+    //console.log($("#right_part").width());
+    $(window).resize(function(){
+        console.log("windows size changed, new size is: " + $("#right_part").width());
+        changeTabListAndMoreTabListWhenWidowsSizeChanged();
+    });
     var username = getUrlParam('name');
     console.log("username: " + username);
     $("#userInfo .userLoginName").html(username);
@@ -26,10 +27,12 @@ $(function () {
               $("#right_part").css("left", "200px");
               $("#left_part").css("left", "0");
           }
+          console.log("Current size is: " + $("#right_part").width());
+          changeTabListAndMoreTabListWhenWidowsSizeChanged();
     });
 
     $("#right_part .tabbed ul").on('click', 'span', function () {
-        console.log("span click.");
+        console.log("tablist span click.");
         var show_nav_list = $("#right_part .tabbed ul li"); 
         var index = show_nav_list.index($(this).parent());
         if ( -1 == index ) {
@@ -41,44 +44,116 @@ $(function () {
         iframe_box.find(".iframe_content").hide().eq(index).show();
     });
 
+    $("#right_part .more_tab ul").on('click', 'span', function () {
+        console.log("more_tab span click.");
+        var show_nav_list = $("#right_part .more_tab ul li");
+        var index = show_nav_list.index($(this).parent());
+        var tablistsize = $("#right_part .tabbed ul li").size();
+        console.log("iframe_box: " + tablistsize + " display.");
+        $("#right_part .show_iframe .iframe_content").hide().eq(tablistsize + index).show();
+    });
+
     $("#right_part .tabbed ul").on('click', '.close', function () {
         var tabIndex = $(this).parent().index();
         var show_nav = $("#right_part .tabbed ul li");
         var iframe_box = $("#right_part .show_iframe");
-
+        var isCurrentTabActive = $(this).parent().hasClass("active");
         console.log('tabIndex:' + tabIndex);
         $(this).parent().remove();
-        if (tabIndex > 0) {
-            iframe_box.find(".iframe_content").eq(tabIndex).remove();
+        iframe_box.find(".iframe_content").eq(tabIndex).remove();
+        if (isCurrentTabActive) {
             show_nav.eq(tabIndex - 1).addClass("active");
             console.log("tab " + (tabIndex - 1) + " has add active class.");
             iframe_box.find(".iframe_content").eq(tabIndex - 1).show();
-        } else if (tabIndex == 0 && show_nav.size() > 0) {
-            console.log("Impossible execute this.");
-            iframe_box.find(".iframe_content").eq(0).remove();
-            show_nav.eq(0).addClass("active");
-            iframe_box.find(".iframe_content").eq(0).show();
-        } else {
-            console.log("Impossible execute this.");
-            var lastIframe = iframe_box.find(".iframe_content").eq(0);
-            lastIframe.removeAttr("src");
-            lastIframe.load();
         }
-
-        // if (isNeedAdjustWidthWhenCloseTab()) {
-        //     adJustWidthWhenCloseTab();
-        // }
+        moveItemFromMoreTabListIntoTabList();
+    });
+    
+    //更多菜单箭头
+    $(".arrow_box").click(function () {
+        console.log("arrow_box");
+        if ($(this).hasClass("down")) {
+            $(".arrow_box").removeClass("down");
+            $(".arrow_box").addClass("up");
+            $(".more_tab").slideDown("1000");
+        } else if ($(this).hasClass("up")) {
+            $(".arrow_box").removeClass("up");
+            $(".arrow_box").addClass("down");
+            $(".more_tab").slideUp("1000");
+        }
     });
 });
 var spanWidthInit = 60;
 var spanWidthChanged = 60;
 var eachLiMarginLeft = 30;
 var eachSpanMargin = 23;
+var marginRight = 50;
 
-function isNeedAdjustWidthWhenAddTab () {
+function changeTabListAndMoreTabListWhenWidowsSizeChanged() {
     var windowSize = $("#right_part").width();
     var eachWidth = spanWidthInit + eachSpanMargin * 2 + eachLiMarginLeft;
-    var maxTabNum = parseInt(windowSize / eachWidth); 
+    var maxTabNum = parseInt((windowSize - marginRight) / eachWidth); 
+    var curTabNum = $("#right_part .tabbed ul").children().size();
+    console.log("windowSize: " + windowSize + ", maxTabNum: " + maxTabNum + ", curTabNum: " + curTabNum);
+    if (maxTabNum < curTabNum) {
+        //减少时
+        for (var index = 0; index < (curTabNum - maxTabNum); ++index) {
+            moveItemFromTabListIntoMoreTabList();
+            console.log("move once time.");
+        }
+    } else if (maxTabNum > curTabNum){
+        //变大时
+        for (var index = 0; index < (maxTabNum - curTabNum); ++index) {
+            moveItemFromMoreTabListIntoTabList();
+            console.log("move once time.");
+        }
+    }
+}
+
+function moveItemFromMoreTabListIntoTabList () {
+    if (!($(".arrow_box").hasClass("down") || $(".arrow_box").hasClass("up"))) {
+        return;
+    }
+    var tabList = $("#right_part .tabbed ul");
+    var moreTabList = $(".more_tab ul li");
+    var moreTabListFirstChild = moreTabList.eq(0);
+    href = moreTabListFirstChild.find('span').attr('page_href');
+    titleName = moreTabListFirstChild.find('span').text();
+    if (href == "home.html") {
+        tabList.append('<li><span class="title" page_href="' + href + '">' + titleName + '</span></li>');
+    } else {
+        tabList.append('<li><span class="title" page_href="' + href + '">' + titleName + '</span><span class="close"><span></li>');
+    }
+    moreTabListFirstChild.remove();
+    if ($(".more_tab ul li").size() == 0) {
+        $(".arrow_box").removeClass("down up");
+        $(".more_tab").hide();
+    }
+}
+
+function moveItemFromTabListIntoMoreTabList () {
+    if (!($(".arrow_box").hasClass("down") || $(".arrow_box").hasClass("up"))) {
+        $(".arrow_box").addClass("down");
+    }
+    var tabList = $("#right_part .tabbed ul li");
+    var moreTabList = $(".more_tab ul li");
+    var tabListLastChild = tabList.eq(tabList.size() - 1);
+    href = tabListLastChild.find('span').attr('page_href');
+    titleName = tabListLastChild.find('span').text();
+    if (moreTabList.size() == 0) {
+        $(".more_tab ul").append('<li><span class="title" page_href="' + href + '">' + titleName + '</span></li>');
+    } else {
+        var moreTabListFirstChild = $(".more_tab ul li:first-child");
+        moreTabListFirstChild.before('<li><span class="title" page_href="' + href + '">' + titleName + '</span></li>');
+    }    
+    tabListLastChild.remove();
+    
+}
+
+function isNeedShowMoreTabWhenAddTab () {
+    var windowSize = $("#right_part").width();
+    var eachWidth = spanWidthInit + eachSpanMargin * 2 + eachLiMarginLeft;
+    var maxTabNum = parseInt((windowSize - marginRight) / eachWidth); 
     var curTabNum = $("#right_part .tabbed ul").children().size();
     console.log("windowSize: " + windowSize + ", eachWidth: " + eachWidth + ", maxTabNum: " + maxTabNum + ", curTabNum: " + curTabNum);
     if (maxTabNum >= curTabNum + 1) {
@@ -88,34 +163,14 @@ function isNeedAdjustWidthWhenAddTab () {
     }
 }
 
-function isNeedAdjustWidthWhenCloseTab () {
-    if (spanWidthChanged == spanWidthInit) {
+function isNeedHideMoreTabWhenCloseTab () {
+    var moreTabListNum = $(".more_tab ul").children().size();
+    if (moreTabListNum > 0) {
         return false;
     } else {
         return true;
     }
 }
-
-function adJustWidthWhenAddTab() {
-    var windowSize = $("#right_part").width();
-    var curTabNum = $("#right_part .tabbed ul").children().size() + 1;
-    var eachTabWidth = parseInt(windowSize / curTabNum);
-    var eachSpanWidth = eachTabWidth - eachSpanMargin * 2 - eachLiMarginLeft;
-    $("#right_part .tabbed ul li span.title").css("width", eachSpanWidth);
-    spanWidthChanged = eachSpanWidth;
-    console.log("windowSize: " + windowSize + ", curTabNum: " + curTabNum + ", eachTabWidth: " + eachTabWidth + ", eachSpanWidth: " + eachSpanWidth);
-}
-
-function adJustWidthWhenCloseTab() {
-    var windowSize = $("right_part").width();
-    var curTabNum = $("#right_part .tabbed ul").children().size() - 1;
-    var eachTabWidth = (windowSize - tabMarginLeft) / curTabNum;
-    var eachSpanWidth = eachTabWidth - eachSpanMargin * 2 - eachLiMarginLeft;
-    $("#right_part .tabbed ul li span.title").css("width", eachSpanWidth);
-    spanWidthChanged = eachSpanWidth;
-    console.log("windowSize: " + windowSize + ", curTabNum: " + curTabNum + ", eachTabWidth: " + eachTabWidth + ", eachSpanWidth: " + eachSpanWidth);
-}
-
 
 function getUrlParam(name){
     //构造一个含有目标参数的正则表达式对象  
@@ -133,44 +188,64 @@ function leftmenu_click(href, title) {
 }
 
 function chooseTab (href, title) {
-    var show_nav_list = $("#right_part .tabbed ul li");
-    var bStop = false;
-    var bStopIndex = 0;
-    show_nav_list.each(function () {
+    var tab_list = $("#right_part .tabbed ul li"); //所有的tab里的标签；
+    var more_tab_list = $("#right_part .more_tab ul li"); //所有的more_tab里的标签；
+    var isFound = false; //查找结果
+    var bStopIndex = 0; //所在位置
+    tab_list.each(function () { //在tab里遍历
         if ($(this).find('span').attr("page_href") == href) {
-            bStop = true;
-            bStopIndex = show_nav_list.index($(this));
-            console.log("bStopIndex: " + bStopIndex);
+            isFound = true;
+            bStopIndex = tab_list.index($(this));
+            console.log("find tad in tab list.");
             return false;
         }
     });
-    if (!bStop) {
+
+    if (!isFound) {
+        more_tab_list.each(function () { //在more_tab里遍历
+            if ($(this).find('span').attr('page_href') == href) {
+                isFound = true;
+                bStopIndex = more_tab_list.index($(this)) + tab_list.size();
+                console.log("find tad in more tab list.");
+                return false;
+            }
+        });
+    };
+
+    if (!isFound) {
+        console.log("Ready to create a iframe.");
         createIframe(href, title);
-    }
-    else {
-        show_nav_list.removeClass("active").eq(bStopIndex).addClass("active");
+    } else {
+        tab_list.removeClass("active");
+        if (tab_list.size() >= (bStopIndex + 1)) {
+            tab_list.eq(bStopIndex).addClass("active");
+        }
         var iframe_box = $("#right_part .show_iframe");
         iframe_box.find(".iframe_content").hide().eq(bStopIndex).show().find("iframe").attr("src", href);
-    }
+    };
 }
 
 function createIframe (href, titleName) {
-    // if (isNeedAdjustWidthWhenAddTab()) {
-    //     adJustWidthWhenAddTab();
-    // }
     var show_nav = $("#right_part .tabbed ul");
+    show_nav.find('li').removeClass("active");
+    if ($(".arrow_box").hasClass("down") || $(".arrow_box").hasClass("up") || isNeedShowMoreTabWhenAddTab()) {
+        if (!($(".arrow_box").hasClass("down") || $(".arrow_box").hasClass("up"))) {
+            $(".arrow_box").addClass("down");
+            console.log("Show the arrow_box.");
+        }
+        console.log("Add item in more_tab.");
+        $(".more_tab ul").append('<li><span class="title" page_href="' + href + '">' + titleName + '</span></li>');
+    } else {
+        //add to tab
+        console.log("Add item in tab list.");
+        show_nav.append('<li class="active"><span class="title" page_href="' + href + '">' + titleName + '</span><span class="close"><span></li>');
+    }
+    
+    //新建iframe_box
     var iframe_box = $("#right_part .show_iframe");
     var iframebox = iframe_box.find(".iframe_content");
-    console.log(show_nav.children().size());
-    if (0 == show_nav.children().size()) {
-        //
-        iframebox.attr("src", href);
-    } else {
-        show_nav.find('li').removeClass("active");
-        iframebox.hide();
-        iframe_box.append('<iframe class="iframe_content" name="iframe_content" src=' + href + ' width="100%" height="100%" frameborder="0" ></iframe>');
-    }
-    show_nav.append('<li class="active"><span class="title" page_href="' + href + '">' + titleName + '</span><span class="close"><span></li>');
+    iframebox.hide();
+    iframe_box.append('<iframe class="iframe_content" name="iframe_content" src=' + href + ' width="100%" height="100%" frameborder="0" ></iframe>');   
     var showbox = iframebox.find(".iframe_content:visible");
     showbox.find("iframe").load();
 }
