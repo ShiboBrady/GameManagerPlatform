e720772fcb7c813fccf6be4586c5959589d1a498
张世柏 <zhangshibo706@gmail.com>
2016-02-23 16:32:38 +0800

    modify data_choose style
    learn from datataking.

--- public/javascript/content_common.js
+++ public/javascript/content_common.js
@@ -55,15 +55,70 @@ $(function () {
         $('.'+$chooseDataType).removeClass("none");
     });
 
-    $(".search-box .arrow").click(function(){
-        if ($(".search-box .datepeaker").hasClass("none1")){
-            $(".search-box .datepeaker").removeClass("none1");
+    $(".search-box .dateshow").click(function(){
+        if ($(".search-box .datepeaker").hasClass("searchdate-none") || $(".search-box .datepeaker").hasClass("none")){
+            $(".search-box .datepeaker").removeClass("searchdate-none");
             $(".search-box .datepeaker").slideDown("1000");
+            if ($(".search-box .datepeaker").hasClass("none")) {
+                $(".search-box .datepeaker").removeClass("none");
+            }
         } else {
-            $(".search-box .datepeaker").addClass("none1");
+            $(".search-box .datepeaker").addClass("searchdate-none");
             $(".search-box .datepeaker").slideUp("1000");
         }
     });
+
+    $(".search-box .datepeaker ul").on('click', 'li', function() {
+        var choosedval = $(this).attr('value');
+        console.log(choosedval);
+        var today = new Date(),
+            startDate,
+            endDate;
+
+        var date = {};
+        strToDate(choosedval, date);
+        startDate = date["startDate"];
+        endDate = date["endDate"];
+        console.log(startDate + ' ' + endDate);
+        $(".search-box .datepeaker").addClass("searchdate-none");
+        $(".search-box .datepeaker").slideUp("1000");
+        dateDisplay(startDate, endDate);
+    });
+
+    $(".search-box .datepeaker .confirm-button .confirm").click(function(){
+        var startDate = $("#begindate").val(),
+            endDate = $("#enddate").val();
+        console.log(startDate + ' ' + endDate);
+        var startDateFormat = startDate.match(/^(\d{2,4})(-|\/)(\d{1,2})\2(\d{1,2})$/); 
+        var endDateFormat = endDate.match(/^(\d{2,4})(-|\/)(\d{1,2})\2(\d{1,2})$/);
+        console.log(startDateFormat);
+        console.log(endDateFormat);
+        if (startDateFormat == null || startDateFormat[3] > 12 || startDateFormat[4] > 31) {
+            $("div.datepeaker .errorinfo .startdateerrormsg").show();
+            return false;
+        } else if (endDateFormat == null || endDateFormat[3] > 12 || endDateFormat[4] > 31) {
+            $("div.datepeaker .errorinfo .enddateerrormsg").show();
+            return false;
+        }
+
+        $(".search-box .datepeaker").addClass("searchdate-none");
+        $(".search-box .datepeaker").slideUp("1000");
+        dateDisplay(startDate, endDate);
+        $(".search-box .datepeaker .more-date input").val("");
+    });
+
+    $(".search-box .datepeaker .confirm-button .cancel").click(function(){
+        $(".search-box .datepeaker .more-date input").val("");
+        $(".search-box .datepeaker").addClass("searchdate-none");
+        $(".search-box .datepeaker").slideUp("1000");
+    });
+
+    $(".search-box .datepeaker .more-date input").focus(function(){
+        $("div.datepeaker .errorinfo .startdateerrormsg").hide();
+        $("div.datepeaker .errorinfo .enddateerrormsg").hide();
+    });
+
+    initDateWhenPageLoad();
 })
 
 function getDate (date) {
@@ -72,67 +127,81 @@ function getDate (date) {
     console.log(activetab);
     var startDate, endDate;
     if ("choosedata_easy" == activetab) {
-        console.log("easy");
         var choosedval = $('.choosedata .dropdown option:selected').val();
         console.log(choosedval);
-        // var d = new Date(2016, 0, 27);
-        // d.setTime(d.getTime()-24*60*60*1000);
         var today = new Date();
-        console.log(today.getDay());
-        
-        switch (choosedval) {
-            case 'yesterday':
-                console.log('yesterday');
-                var yestd = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
-                yestd = yestd.getFullYear() + '-' + (yestd.getMonth() + 1) + '-' + yestd.getDate();
-                console.log("yesterday is: " + yestd);
-                startDate = yestd;
-                endDate = yestd;
-                break;
-            case 'thisweek':
-                console.log('thisweek');
-                var workday = today.getDay();
-                var monDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() - workday + 1);
-                startDate = monDay.getFullYear() + '-' + (monDay.getMonth() + 1) + '-' + monDay.getDate();
-                endDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-                break;
-            case 'lastweek':
-                console.log('lastweek');
-                var workday = today.getDay();
-                var lastMonDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() - workday - 6);
-                var lastSunDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() - workday);
-                startDate = lastMonDay.getFullYear() + '-' + (lastMonDay.getMonth() + 1) + '-' + lastMonDay.getDate();
-                endDate = lastSunDay.getFullYear() + '-' + (lastSunDay.getMonth() + 1) + '-' + lastSunDay.getDate();
-                break;
-            case 'lastthiryday':
-                console.log('lastthiryday');
-                var thiryDayBefore = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30 + 1);
-                console.log(thiryDayBefore);
-                startDate = thiryDayBefore.getFullYear() + '-' + (thiryDayBefore.getMonth() + 1) + '-' + thiryDayBefore.getDate();
-                endDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-                break;
-            case 'lastmonth':
-                console.log('lastmonth');
-                var lastMonthStart = new Date(today.getFullYear(), today.getMonth() - 1, 1);
-                console.log(lastMonthStart);
-                var thisMonthStart = new Date(today.getFullYear(), today.getMonth(), 1);
-                var lastMonthEnd = new Date(thisMonthStart.getFullYear(), thisMonthStart.getMonth(), thisMonthStart.getDate() - 1);
-                console.log(lastMonthEnd);
-                startDate = lastMonthStart.getFullYear() + '-' + (lastMonthStart.getMonth() + 1) + '-' + lastMonthStart.getDate();
-                endDate = lastMonthEnd.getFullYear() + '-' + (lastMonthEnd.getMonth() + 1) + '-' + lastMonthEnd.getDate();
-                break;
-            default:
-                startDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();;
-                endDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();;
-        }
+        strToDate(choosedval, date);
     } else {
-        startDate = $("#begindate").val();
-        endDate = $("#enddate").val();
+        date["startDate"] = $("#begindate").val();
+        date["endDate"] = $("#enddate").val();
     }
-    console.log(startDate);
-    console.log(endDate);
+}
+
+function strToDate (strDate, date) {
+    var today = new Date(),
+        startDate,
+        endDate;
+
+    switch (strDate) {
+        case 'yesterday':
+            console.log('yesterday');
+            var yestd = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
+            yestd = formatDate(yestd);
+            console.log("yesterday is: " + yestd);
+            startDate = yestd;
+            endDate = yestd;
+            break;
+        case 'thisweek':
+            console.log('thisweek');
+            var workday = today.getDay();
+            var monDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() - workday + 1);
+            startDate = formatDate(monDay);
+            endDate = formatDate(today);
+            break;
+        case 'lastweek':
+            console.log('lastweek');
+            var workday = today.getDay();
+            var lastMonDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() - workday - 6);
+            var lastSunDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() - workday);
+            startDate = formatDate(lastMonDay);
+            endDate = formatDate(lastSunDay);
+            break;
+        case 'lastthiryday':
+            console.log('lastthiryday');
+            var thiryDayBefore = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30 + 1);
+            console.log(thiryDayBefore);
+            startDate = formatDate(thiryDayBefore);
+            endDate = formatDate(today);
+            break;
+        case 'lastmonth':
+            console.log('lastmonth');
+            var lastMonthStart = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+            console.log(lastMonthStart);
+            var thisMonthStart = new Date(today.getFullYear(), today.getMonth(), 1);
+            var lastMonthEnd = new Date(thisMonthStart.getFullYear(), thisMonthStart.getMonth(), thisMonthStart.getDate() - 1);
+            console.log(lastMonthEnd);
+            startDate = formatDate(lastMonthStart);
+            endDate = formatDate(lastMonthEnd); 
+            break;
+        default:
+            startDate = formatDate(today);
+            endDate = formatDate(today);
+    }
+
     date["startDate"] = startDate;
     date["endDate"] = endDate;
+}
+
+function formatMonthAndDay(value){
+    if (value < 10) {
+        return '0' + value;
+    } else {
+        return value.toString();
+    }
+}
+
+function formatDate(date){
+    return date.getFullYear() + '-' + formatMonthAndDay(date.getMonth() + 1) + '-' + formatMonthAndDay(date.getDate());
 }
 
 var date = {};
@@ -146,4 +215,15 @@ function search (data_kind) {
         url: '/data/' + data_kind,
         success: callbacksuccess,
     })
+}
+
+function dateDisplay(startDate, endDate) {
+    $(".display-startdate").html(startDate);
+    $(".display-enddate").html(endDate);
+}
+
+function initDateWhenPageLoad() {
+    var date = {};
+    strToDate('today', date);
+    dateDisplay(date['startDate'], date['endDate']);
 }
